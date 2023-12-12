3fc42abc4ca2599da8eab745ddfa13d50c17bcb8
Flrdlz <153481471+Flrdlz@users.noreply.github.com>
2023-12-12 20:47:51 +0800

    Add files via upload

---
+++ Pseudocode.py
@@ -0,0 +1,82 @@
+def what_to_tap()
+
+    if input/touch == a current contact
+        call the current contact() method
+    
+    elif input/touch == exit button
+        break
+
+    elif input/touch == add contact button
+        call
+
+    else:
+        
+
+
+def current_contact()
+    display the info of the selected contact, edit contact button and back button
+
+    input/touch
+    if input/touch == back button
+        call main() method
+    elif input/touch == edit contacyt button
+        call edit_contact() method
+
+def edit_contact()
+    display the list that must be filled to edit the contact and cancel button
+
+    input/touch
+    if input/touch == cancel button
+        call current_contact() method
+
+    elif input/touch == user edited the info
+        display save button and discard button
+
+        input/touch
+        if input/touch ==  save button
+            save edited contact info
+            call main() method
+        else:
+            dont save edited info
+            call current_contact() method
+
+    else:
+
+
+
+def add_contact()
+    display the list that must be filled to add new contact and cancel button
+
+    input/touch
+    if input/touch == cancel button 
+        call main method
+    elif input/touch == user inputs new info 
+        display save button and cancel button
+
+        input/touch
+        if input/touch == save button
+            save new contact
+            call main() method       
+        else:
+            Dont save new contact
+            call main() method 
+    else:
+    
+
+
+start 
+Initializing app
+
+def main()
+display current contacts, add contact button, and exit button
+
+input/touch
+call what_to_tap method
+end
+
+
+
+
+
+
+
